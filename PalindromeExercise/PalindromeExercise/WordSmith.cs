a4134ad112f8cea1567863c083c05ae8c1486f8d
bdurushia <b.durushia@gmail.com>
2024-03-12 08:47:31 -0500

    Completed Palindrome Tests and method. (Unit Test Exercise 2).

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (string.IsNullOrEmpty(word)) return false;
+            
+            var reversedWord = new string(word.Reverse().ToArray()).ToLower();
+
+            if (reversedWord == word.ToLower())
+                return true;
+            else
+                return false;
+                
+        }
+    }
+}
